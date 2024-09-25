2df9d0499016858c19b3c2295e57b9bb0b0096eb
Manmeet <manmeet7210@gmail.com>
2024-09-25 16:56:13 +0530

    Context-API added

---
+++ React/theme-switcher/src/App.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './context/theme'
+import Theme from './components/Theme'
+import Card from './components/card'
+
+function App() {
+  const [themeMode,setThemeMode]=useState('light')
+
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+
+  //actual change  in theme
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
+  return (
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <Theme></Theme>
+                </div>
+                <div className="w-full max-w-sm mx-auto">
+                  <Card></Card>
+                </div>
+            </div>
+        </div>
+      </>
+    </ThemeProvider>
+  )
+}
+
+export default App
